52aded5684010e95bc91ae19e78a71f955c5e9bd
nfagan <nick@fagan.org>
2018-05-17 15:43:37 -0400

    add time output

--- +ms_run/make_mda_file.m
+++ +ms_run/make_mda_file.m
@@ -1,4 +1,4 @@
-function make_mda_file( pl2_file, channels, out_file )
+function t = make_mda_file( pl2_file, channels, out_file )
 
 import shared_utils.assertions.*;
 
@@ -11,6 +11,7 @@ for i = 1:numel(channels)
   
   if ( i == 1 )
     mat = zeros( numel(channels), numel(ad.Values), 'single' );
+    t = (0:numel(ad.Values)-1) * (1/ad.ADFreq);
   end
   
   mat(i, :) = single( ad.Values );
